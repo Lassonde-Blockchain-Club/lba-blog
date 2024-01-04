1898bdc2ec3ad152dedae8b3621528337ae4c02f
Ayaan <mayaan0606@gmail.com>
2024-01-04 13:27:26 -0500

    gradient added

--- app/(site)/login-page/page.tsx
+++ app/(site)/login-page/page.tsx
@@ -4,10 +4,20 @@ import React from "react";
 const Login: React.FC = () => {
   return (
     <div className="flex h-screen">
-      <div className="flex-1 items-center justify-center text-center bg-gray-200">
+      <div
+        className="flex-1 items-center justify-center text-center"
+        style={{
+          background: "linear-gradient(to right, #ff7e5f, #feb47b)",
+        }}
+      >
         <div className="text-4xl font-bold">Left</div>
       </div>
-      <div className="flex-1 items-center justify-center text-center bg-gray-300">
+      <div
+        className="flex-1 items-center justify-center text-center"
+        style={{
+          background: "linear-gradient(to right, #ff7e5f, #feb47b)",
+        }}
+      >
         <div className="text-4xl font-bold">Right</div>
       </div>
     </div>
