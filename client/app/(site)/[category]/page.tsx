1c8b7048371fd61ac8647aab6a853a607afd71ae
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2024-03-11 18:08:05 -0400

    Update page.tsx
    added pagination and made it responsive

--- client/app/(site)/[category]/page.tsx
+++ client/app/(site)/[category]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import React, { useState } from "react";
 import SmallBlogCard from "../components/SmallBlogCard";
+import Pagination from "../components/Pagination";
 
 const defaultCategoryTitle = "AI / Machine Learning";
 const defaultCategoryDescription =
@@ -29,10 +31,20 @@ const exampleBlog: SmallBlog = {
   mollit anim id est laborum.",
 };
 
-var exampleBlogs = new Array(10);
+var exampleBlogs = new Array(45);
 exampleBlogs.fill(exampleBlog);
 
 export default function CategoryPage() {
+  const blogsPerPage = 9; 
+  const [currentPage, setCurrentPage] = useState(1);
+
+
+  const currentBlogs = exampleBlogs.slice(currentPage * blogsPerPage - blogsPerPage, currentPage * blogsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="">
       <div className="absolute -z-10">
@@ -46,21 +58,35 @@ export default function CategoryPage() {
       <div className="container h-full w-full pt-20">
         <div className="my-32 w-8/12">
           <h1 className="text-6xl font-semibold">{defaultCategoryTitle}</h1>
-          <p className="m-2 mt-5 w-6/12">{defaultCategoryDescription}</p>
+          {/* <p className="m-2 mt-5 w-6/12">{defaultCategoryDescription}</p> */}
+          <p className="m-2 mt-5 w-full sm:w-6/12">{defaultCategoryDescription}</p> 
         </div>
-        <div className="grid grid-cols-3 gap-4 gap-y-16 justify-center items-center">
-          {exampleBlogs.map((blog, index) => Blog(blog, index))}
+        {/* <div className="grid grid-cols-3 gap-4 gap-y-16 justify-center items-center"> */}
+        <div className="grid grid-cols-1 gap-4 gap-y-16 justify-center items-center sm:grid-cols-2 md:grid-cols-3">
+          {currentBlogs.map((blog, index) => (
+            <Blog key={index} blog={blog} index={index} />
+          ))}
         </div>
+        <Pagination
+          currentPage={currentPage}
+          numTotalPages={Math.ceil(exampleBlogs.length / blogsPerPage)}
+          onChangePage={handlePageChange}
+        />
       </div>
     </div>
   );
 }
 
-function Blog(blog: SmallBlog, index: number) {
-  var blogClassName: string;
-  if (index % 3 == 0) {
+interface BlogProps {
+  blog: SmallBlog;
+  index: number;
+}
+
+function Blog({ blog, index }: BlogProps) {
+  let blogClassName: string;
+  if (index % 3 === 0) {
     blogClassName = "justify-start";
-  } else if (index % 3 == 1) {
+  } else if (index % 3 === 1) {
     blogClassName = "justify-center";
   } else {
     blogClassName = "justify-end";
